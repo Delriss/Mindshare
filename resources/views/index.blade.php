9c2f5448a342f50b055ba28d1595664665666012
Delriss <haxz3rz@gmail.com>
2024-11-04 15:55:16 +0000

    General Index Changes and Dark Mode implementation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,16 @@
 <x-app-layout>
-<x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 text-center dark:text-gray-200 leading-tight">
+    <x-slot name="header">
+        <img src="{{ asset('images/brain.png') }}" class="mx-auto hover:scale-105 transition-transform duration-200 w-250 h-250">
+        <h2 class="mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl lg:text-6xl dark:text-white hover:scale-105 transition-transform duration-200 max-w-fit mx-auto">
             {{ __('Welcome to Mindshare!') }}
         </h2>
+        <p class="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8 text-center">Your hub for knowledge and inspiration.</p>
+        <div class="mt-10 flex items-center justify-center gap-x-6"><a href="{{ route('register') }}" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a><a href="#" class="text-sm/6 font-semibold text-gray-900 dark:text-white">Learn more <span aria-hidden="true">→</span></a></div>
     </x-slot>
 
     <div class="container mx-auto py-12 space-y-10 flex flex-col items-center text-center">
         <div class="block max-w-3xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ __('Welcome to MindShare') }}</h3>
+            <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ __('Welcome to Mindshare') }}</h3>
             <p class="font-normal text-gray-700 dark:text-gray-400">{{ __('Your hub for knowledge and inspiration.') }}</p>
 
             <p class="font-normal text-gray-700 dark:text-gray-400">{{ __('We\'re a community-driven blog where individuals from all walks of life come together to share their unique perspectives and insights. Whether you\'re an expert in your field or simply passionate about a particular topic, you\'ll find a welcoming space to contribute your voice.') }}</p>
