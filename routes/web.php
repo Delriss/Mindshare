90360a828b32902071165c003a93d893ba0ee1bc
Delriss <haxz3rz@gmail.com>
2024-11-05 20:59:44 +0000

    Fix for Auth errors and implementation of card changes in dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,9 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [SiteController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [SiteController::class, 'index'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,9 +15,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::controller(BlogController::class) -> group(function () {
+Route::controller(BlogController::class) -> middleware(['auth', 'verified']) -> group(function () {
     Route::get('/dashboard', [BlogController::class, 'index'])->name('dashboard');
     Route::get('/{blogPost}', [BlogController::class, 'show'])->name('blog.show');
 });
-
-require __DIR__.'/auth.php';
